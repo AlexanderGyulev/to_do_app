6dc17452e574ea9529b179e9ea8575f6d7ae6a90
AlexanderGyulev <104764256+AlexanderGyulev@users.noreply.github.com>
2026-02-01 16:17:18 +0200

    commit changes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
+from ui import ToDoApp
 
 class TaskManager:
     def __init__(self):
         self.task_list = []
-        
+    def add_task(self,text):
+        self.task_list.append(text)
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    app = TaskManager()
+    manager = TaskManager()
+    app = ToDoApp(root, manager)
     root.mainloop()
--- ui.py
+++ ui.py
@@ -6,3 +6,21 @@ class ToDoApp:
         self.task_manager = task_manager
         self.root.title("To Do App")
         self.root.geometry("600x600")
+        self.build_ui()
+    def build_ui(self):
+        self.text_entry = tk.Entry(self.root)
+        self.text_entry.pack()
+        self.add_button = tk.Button(text="Add a task", command=self.on_add)
+        self.add_button.pack()
+        self.task_listbox = tk.Listbox(self.root)
+        self.task_listbox.pack()
+        self.delete_button = tk.Button(text="Delete a task")
+        self.delete_button.pack()
+        self.status_label = tk.Label(self.root, text="")
+        self.status_label.pack()
+    def on_add(self):
+        text = self.text_entry.get()
+        self.task_manager.add_task(text)
+        self.refresh_list()
+    def refresh_list(self):
+        pass
